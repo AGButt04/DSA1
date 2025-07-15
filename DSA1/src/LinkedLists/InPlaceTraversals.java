2bc68724f5ca17076e60c9a0059f9dd6e01d53e2
Abdul Ghani Butt <agbutt@widener.edu>
2025-07-15 00:25:21 -0400

    Added evenOddList re-order and Adding two lists (Medium)

--- DSA1/src/LinkedLists/InPlaceTraversals.java
+++ DSA1/src/LinkedLists/InPlaceTraversals.java
@@ -1,5 +1,7 @@
 package LinkedLists;
 
+import java.util.Stack;
+
 public class InPlaceTraversals {
 
 	public static void main(String[] args) {
@@ -14,13 +16,118 @@ public class InPlaceTraversals {
 		four.next = null;
 //		five.next = null;
 		
-		ListNode newHead = swapPairs(head);
+		ListNode newHead = evenoddList(head);
 		while (newHead != null) {
 			System.out.print(newHead.val + " ");
 			newHead = newHead.next;
 		}
 		System.out.println();
 
+	}
+	
+	public static ListNode addtwonumbers(ListNode l1, ListNode l2) {
+		/*
+		 * Leet-code 445 (Medium)
+		 */
+		/*
+		 * Using stack to add elements for both lists, left-to-right.
+		 * Then Popping the stack and adding elements and creating another
+		 * lists with the sum. Have to take care of carry.
+		 */
+		Stack<Integer> st1 = new Stack<>();
+		Stack<Integer> st2 = new Stack<>();
+		ListNode walker1 = l1;
+		ListNode walker2 = l2;
+		while (walker1 != null) {
+			st1.add(walker1.val);
+			walker1 = walker1.next;
+		}
+		
+		while (walker2 != null) {
+			st2.add(walker2.val);
+			walker2 = walker2.next;
+		}
+		
+		
+		ListNode newhead = new ListNode(0);
+		ListNode curr = newhead;
+		int carry = 0;
+		while (!st1.isEmpty() && !st2.isEmpty()) {
+			int num1 = st1.pop();
+			int num2 = st2.pop();
+			int sum = num1 + num2;
+			sum += carry;
+			if (sum > 9) {
+				carry = sum / 10;
+				sum = sum % 10;
+			} else
+				carry = 0;
+			ListNode newNode = new ListNode(sum);
+			curr.next = newNode;
+			curr = curr.next;
+		}
+		if (st1.isEmpty()) {
+			while (!st2.isEmpty()) {
+				int num = st2.pop();
+				if (carry > 0) {
+					num += carry;
+					carry = num / 10;
+					num = num % 10;
+				}
+				ListNode node = new ListNode(num);
+				curr.next = node;
+				curr = curr.next;
+			}
+		} else if (st2.isEmpty()) {
+			while (!st1.isEmpty()) {
+				int num = st1.pop();
+				if (carry > 0) {
+					num += carry;
+					carry = num / 10;
+					num = num % 10;
+				}
+				ListNode node = new ListNode(num);
+				curr.next = node;
+				curr = curr.next;
+			}
+		}
+		
+		if (carry > 0) {
+			ListNode carryNode = new ListNode(carry);
+			curr.next = carryNode;
+		}
+		// As the list was build right-to-left, we have to
+		// reverse it in order to get the right output.
+		return reverseList(newhead.next);
+	}
+	
+	public static ListNode evenoddList(ListNode head) {
+		/*
+		 * Leet-code 328 (medium)
+		 */
+		/*
+		 * The idea is having 2 nodes pointing at 1st and 2nd
+		 * nodes of the list and just start connecting them accordingly.
+		 * Just keep track of the last node of the odd nodes' walker.
+		 */
+		ListNode head2 = head.next;
+		ListNode walker1 = head;
+		ListNode walker2 = head2;
+		
+		// Keep going until there are one node present in front of both pointers
+		while (walker1.next != null && walker2.next != null) {
+			walker1.next = walker1.next.next;
+			walker2.next = walker2.next.next;
+			walker1 = walker1.next;
+			walker2 = walker2.next;
+		}
+		
+		// At the end, we know that walker1 pointer points to the last node
+		// of the odd nodes so we just connect it to the head of even nodes.
+		walker1.next = head2;
+		
+		// Return the head.
+		return head;
 	}
 	
 	public static ListNode swapPairs(ListNode head) {
