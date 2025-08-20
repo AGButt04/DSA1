745b607cb1dcd295a55c81050e75fc63fa8b7fca
Abdul Ghani Butt <agbutt@widener.edu>
2025-08-20 00:06:43 -0400

    Word Search in a Grid (MEDIUM)

--- DSA1/src/Recursion/Backtracking.java
+++ DSA1/src/Recursion/Backtracking.java
@@ -19,6 +19,36 @@ public class Backtracking {
 
     }
 
+    public static boolean exist(char[][] board, String word) {
+        /*
+        Leet-code 79 (Medium)
+        */
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if (backtrack(board, word, i, j, 0)) 
+                    return true;
+            }
+        }
+        return false;
+    }
+    public static boolean backtrack(char[][] board, String word, int i, int j, int index) {
+        if (index == word.length()) 
+            return true; // If we reached the end of the word
+        if (i < 0 || i >= board.length || j < 0 || j >= board[0].length || board[i][j] != word.charAt(index)) 
+            return false;
+
+        char temp = board[i][j];
+        board[i][j] = '#'; // Mark as visited
+
+        boolean found = backtrack(board, word, i + 1, j, index + 1) ||
+                        backtrack(board, word, i - 1, j, index + 1) ||
+                        backtrack(board, word, i, j + 1, index + 1) ||
+                        backtrack(board, word, i, j - 1, index + 1);
+
+        board[i][j] = temp; // Restore original value
+        return found;
+    }
+
     public List<String> letterCombinations(String digits) {
         /*
         Leet-code 17 (Medium)
