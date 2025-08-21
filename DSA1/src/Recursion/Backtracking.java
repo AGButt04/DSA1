73520ab0cf98e5e6fd47a6ddf4e7dfec446187e7
Abdul Ghani Butt <agbutt@widener.edu>
2025-08-21 10:05:26 -0400

    Descriptive Comments for all problems so far

--- DSA1/src/Recursion/Backtracking.java
+++ DSA1/src/Recursion/Backtracking.java
@@ -2,6 +2,7 @@ package Recursion;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 
 public class Backtracking {
@@ -22,7 +23,10 @@ public class Backtracking {
 
     public List<List<String>> partition(String s) {
         /*
-        Leet-code 131
+        Leet-code 131 (Medium)
+        In this problem, we are finding all the partitions of the string
+        s which are palindrome. All the partitions have to be palindromes.
+        For this purpose, we can recursion and backtracking to generate all.
          */
         if  (s == null || s.length() == 0) return new ArrayList<>();
         List<List<String>> partitions = new ArrayList<>();
@@ -30,20 +34,30 @@ public class Backtracking {
         return partitions;
     }
     public void backtrackPartition(List<List<String>> partitions, List<String> list, String s, int start){
+        // If the start index has reached the length of the String s
+        // that means we have a valid partitioning of the palindrome
         if (start == s.length()){
             partitions.add(new ArrayList<>(list));
             return;
         }
+        /*
+        The idea here is that we have to recurse for all of the partitions of the
+        String s, of length 1, 2, ... , s.length. And we have to recurse of all substrings.
+        We will start the for loop at the index start which will go till end == s.length()
+         */
         for (int end = start+1; end < s.length(); end++){
-            String substring = s.substring(start, end);
-            if (isPalindrome(substring)) {
-                list.add(substring);
-                backtrackPartition(partitions, list, s, end);
-                list.remove(list.size()-1);
+            String substring = s.substring(start, end); // Get a substring from start to end.
+            if (isPalindrome(substring)) { // Check, if it is a Palindrome, else continue.
+                list.add(substring); // Add that string to our current running list we're working with.
+                backtrackPartition(partitions, list, s, end); // Recurse with the modified current list
+                list.remove(list.size()-1); // Remove the element added (backtrack) for other calls.
             }
         }
     }
     public boolean isPalindrome(String s){
+        /*
+        This is a simple palindrome checker for above question.
+         */
         int left = 0;
         int right = s.length()-1;
         while (left < right){
@@ -59,77 +73,104 @@ public class Backtracking {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
         /*
         Leet-code 90 (Medium)
+        This problem is the same as generating duplicates but we here
+        have duplicate elements is the array given to deal with. We can
+        still use the same backtracking approach with some conditions.
          */
         List<List<Integer>> subsets = new ArrayList<>();
-        Arrays.sort(nums);
+        Arrays.sort(nums); // Sort the array for duplicate handling.
         backtrackSubsets(subsets, new ArrayList<Integer>(), nums, 0);
         return subsets;
     }
     public static void backtrackSubsets(List<List<Integer>> subsets, List<Integer> list, int[] nums, int index){
+        // Add the subset to our subsets LIST as all combinations are valid
         subsets.add(new ArrayList<>(list));
+        // Start the loop from the index given which will start from 0
         for (int i = index; i < nums.length; i++) {
+            // Here, we check if the current element is the same as the previous
+            // then we continue as arrays are sorted, all duplicates will be adjacent.
             if (i > index && nums[i] == nums[i - 1]) continue;
-            list.add(nums[i]);
-            backtrackSubsets(subsets, list, nums, i + 1);
-            list.remove(list.size() - 1);
+
+            list.add(nums[i]); // Add the current element of the array in the set
+            backtrackSubsets(subsets, list, nums, i + 1); // Recurse with the new set and next index (next element)
+            list.remove(list.size() - 1); // Remove the current element after exploring that path for future computations.
         }
     }
 
     public static boolean exist(char[][] board, String word) {
         /*
         Leet-code 79 (Medium)
+        Here, we are finding if the word exists in our grid or not.
+        This also can be done with BFS, but we are choosing backtracking.
         */
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[0].length; j++) {
+                // We will have to scan each element as the potential starting point
+                // where the word could exists, and start recursion there. If our
+                // backtrack method returns true, that means we found it, so we return true.
                 if (backtrack(board, word, i, j, 0))
                     return true;
             }
         }
+        // If we did not find it, return false at the end.
         return false;
     }
     public static boolean backtrack(char[][] board, String word, int i, int j, int index) {
-        if (index == word.length()) 
-            return true; // If we reached the end of the word
+        if (index == word.length())  // Base case
+            return true; // If we reached the end of the word (Found it!)
+        // Then we check the bounds, if the indexes got out of bounds, just return false.
         if (i < 0 || i >= board.length || j < 0 || j >= board[0].length || board[i][j] != word.charAt(index)) 
             return false;
 
-        char temp = board[i][j];
-        board[i][j] = '#'; // Mark as visited
+        char temp = board[i][j]; // Here we are storing the current element to use again.
+        board[i][j] = '#'; // Mark as visited so that it won't go down this path again.
 
+        /*
+        Then, the idea here is that there are 4 directions to check from a current
+        index -> (left, right, up, down), and word could be in any of these directions.
+        So, we recurse in four directions and store their result in one boolean variable
+        which are connected by OR because if any of these calls return true, we return true (FOUND IN THAT DIRECTION)
+         */
         boolean found = backtrack(board, word, i + 1, j, index + 1) ||
                         backtrack(board, word, i - 1, j, index + 1) ||
                         backtrack(board, word, i, j + 1, index + 1) ||
                         backtrack(board, word, i, j - 1, index + 1);
 
         board[i][j] = temp; // Restore original value
-        return found;
+        return found; // Return if found it or not.
     }
 
     public List<String> letterCombinations(String digits) {
         /*
         Leet-code 17 (Medium)
+        Here, we have to generate all letter combinations of the integers
+        given as a string, and we can use a HashMap to store each word's
+        mapping and can use StringBuilder to create mappings and check.
         */
         if (digits.equals("")) return new ArrayList<>();
         
         List<String> combinations = new ArrayList<>();
-        HashMap<Character, String> mappings = map();
-        backtrack(combinations, mappings, new StringBuilder(), digits, 0);
+        HashMap<Character, String> mappings = map(); // Generate the mappings from the function below.
+        backtrack(combinations, mappings, new StringBuilder(), digits, 0); // Populate the combinations
         return combinations;
     }
     public void backtrack(List<String> combinations, HashMap<Character, String> mappings,
                          StringBuilder current, String digits, int index) {
+        // Base case: If our current mapping is the length of the
+        // String digits, then we add it to our combinations.
         if (current.length() == digits.length()) {
             combinations.add(current.toString());
             return;
         }
 
+        //Here, we will go into the loop for each digit in th digits array.
         for (int i = index; i < digits.length(); i++) {
-            char ch = digits.charAt(i);
-            String mapping = mappings.get(ch);
-            for (char c : mapping.toCharArray()) {
-                current.append(c);
-                backtrack(combinations, mappings, current, digits, i+1);
-                current.deleteCharAt(current.length()-1);
+            char ch = digits.charAt(i); // Get the digit at the current index.
+            String mapping = mappings.get(ch); // Retrieve its mappings from the map.
+            for (char c : mapping.toCharArray()) { // Go over all of its mappings
+                current.append(c); // Append the character to our current Stringbuilder to create a new combo
+                backtrack(combinations, mappings, current, digits, i+1); // Recurse forward with that combo to explore the path
+                current.deleteCharAt(current.length()-1); // Delete that character to backtrack and for future.
             }
         }
     }
