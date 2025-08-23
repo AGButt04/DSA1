033ccaab93beecfdd922dc5c10ea9a08dea97459
Abdul Ghani Butt <agbutt@widener.edu>
2025-08-23 17:07:09 -0400

    CoinChange and UniquePaths in a grid (Medium)

--- DSA1/src/Recursion/DP.java
+++ DSA1/src/Recursion/DP.java
@@ -1,5 +1,6 @@
 package Recursion;
 
+import java.util.Arrays;
 import java.util.HashMap;
 
 public class DP {
@@ -8,6 +9,145 @@ public class DP {
         System.out.println(fib_memo(5));
         System.out.println(climbStairs(5));
         System.out.println(climbStairs_Tab(5));
+    }
+
+    public static int uniquePaths_tabulation(int m, int n) {
+        /*
+        Leet-code 62 (Tabulation)
+        In this problem, we are using tabulation to solve the uniquePaths in
+        a (m x n) grid. The idea is we will use a m x n grid to compute small
+        solutions step-by-step and eventually, we will make up the final solution.
+         */
+        // Paths grid where each cell will have the unique paths that
+        // you can find in (i x j) grid, and eventually we will get (m x n).
+        int[][] grid = new int[m][n];
+
+        // The idea here is that we will fill the first column and first
+        // row of the grid with ones, as there is only one way to traverse
+        // the grids which either have 1 row or 1 column.
+        for (int i = 0; i < m; i++)
+            grid[i][0] = 1;
+        for (int j = 0; j < n; j++)
+            grid[0][j] = 1;
+
+        // Scanning through the paths grid to fill in the remaining values.
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                /*
+                Then the main logic says that the uniquePaths for any cells is the sum
+                of the unique paths from its upper cell and left cell, because the problem
+                says that you can only move right and down, so we will only sum those two cells.
+                 */
+                int sum = grid[i-1][j] + grid[i][j-1];
+                grid[i][j] = sum; // Place the sum at that cell
+            }
+        }
+        // Return the last index of the grid as that will have the unique paths for (m x n) grid.
+        return grid[m-1][n-1];
+    }
+
+    public int uniquePaths(int m, int n) {
+        /*
+        Leet-code 62 (Memoization)
+        This is the slow version but faster than the brute force recursion solution,
+        as here we have a memo object which is our hashmap that will store the computations.
+         */
+        return uniquePaths(m, n, new HashMap<String, Integer>());
+    }
+    public int uniquePaths(int m, int n, HashMap<String, Integer> memo) {
+        // We will store the String key in the map -> mapped to its unique paths
+        String curr = m + "," + n;
+        // First, we see if our current dimensions exist in our map or not.
+        if (memo.containsKey(curr)) return memo.get(curr);
+        // Base case: If the row and col == 1, then return 1
+        // because there is only 1 way to traverse 1 x 1 grid
+        if (m == 1 && n == 1) return 1;
+        // If the dimension go beyond 0, then just return 0 invalid dimensions.
+        if (m <= 0 || n <= 0) return 0;
+
+        /*
+        The idea here is that we can only go down and right, and we know that
+        going right decreases the columns by 1 and going down decreases the
+        rows by one, so that is what we will do and change the dimensions to
+        eventually get to our base case which is 1 x 1 grid.
+         */
+        int right = uniquePaths(m, n - 1, memo);
+        int down = uniquePaths(m - 1, n, memo);
+
+        int result = right + down; // Add the results
+        memo.put(curr, result); // Put the results with the key
+        return result; // return the result.
+    }
+
+    public int coinChange_Tabulation(int[] coins, int amount) {
+        /*
+        Leet-code 322 (Tabulation)
+        In this problem, we are using tabulation to solve this problem where we
+        are trying to make the amount given with the coins given to us.
+         */
+        if (amount == 0) return 0; // Edge case, if amount == 0, just return 0
+
+        /*
+        Here, we will make a min_coins array which will be amount+1 in length and
+        we will start filling it up till the amount index which will eventually
+        have the answer to the amount given, and each index in the array will have
+        the minimum coins that will sum up to that amount (index).
+         */
+        int[] min_coins = new int[amount+1];
+        Arrays.fill(min_coins, amount+1); // We will fill array with (amount+1) as default
+        min_coins[0] = 0; // We will put 0 at first index as 0 coins are needed to make 0.
+
+        // Filling up the array by starting from the 1st index.
+        for (int i = 1; i <= amount; i++) {
+            // For each index, we will go over all the coins to get the
+            // minimum coins needed to make this i amount, which will help.
+            for (int coin : coins) {
+                // Remaining amount left after subtracting this current coin from i amount.
+                int remaining_amount = i - coin;
+                // If the remaining is -ve, then don't do anything.
+                if (remaining_amount >= 0) {
+                    // Then the next minimum would be the minimum of the current minimum that
+                    // is at the index i in array, and minimum we calculated which is
+                    // min_coins[remaining_amount]: Get the minimum coins for this element from array
+                    // Add one to it because that will include the current coin we're considering.
+                    int next_min = Math.min(min_coins[i], min_coins[remaining_amount]+1);
+                    min_coins[i] = next_min; // Put the minimum of these at the current index.
+                }
+            }
+        }
+        // Return the min_coins[amount] if its is not == to amount+1, which
+        // was default, if that did not change that means, we couldn't find
+        // the solution, so return -1 in that case.
+        return min_coins[amount] == amount+1? -1 : min_coins[amount];
+    }
+
+    public static int coinChange(int[] coins, int amount) {
+        /*
+        Leet-code 322 (Memoized)
+        This is the memoized version of the coinChange which is implemented
+        on the same concept of going over all the coins and find the minimum.
+         */
+        int result = coinChangeHelper(coins, amount, new HashMap<Integer, Integer>());
+        return result == Integer.MAX_VALUE? -1 : result;
+    }
+    public static int coinChangeHelper(int[] coins, int amount, HashMap<Integer, Integer> memo) {
+        if (amount == 0) return 0; // If we found it return 0.
+        if (amount < 0) return Integer.MAX_VALUE; // If went beyond, return Integer.MAX_VALUE.
+        if (memo.containsKey(amount)) return memo.get(amount); // Added memoization for faster solution.
+
+        int min_coins = Integer.MAX_VALUE; // Default min_coins.
+        for (int coin : coins) { // Going over all the coins
+            // Recursively call the method on amount-coin to get the coins for it.
+            int curr_coins = coinChangeHelper(coins, amount - coin, memo);
+            if (curr_coins != Integer.MAX_VALUE) {
+                // If it found it and did not return Integer.MAX_VALUE, the we can
+                // update our min_coins for this iteration and add 1 to it as current coin.
+                min_coins = Math.min(curr_coins + 1, min_coins);
+            }
+        }
+        // Put the solution in the map with amount as the key.
+        memo.put(amount, min_coins);
+        return min_coins; // return the solution.
     }
 
     public static int rob_Tabulation(int[] nums) {
