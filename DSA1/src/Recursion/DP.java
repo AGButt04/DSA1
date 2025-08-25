550acdf27fa7bd562ee594d1d83ca9698a999e9b
Abdul Ghani Butt <agbutt@widener.edu>
2025-08-24 20:18:01 -0400

    Length of common subsequence in two strings

--- DSA1/src/Recursion/DP.java
+++ DSA1/src/Recursion/DP.java
@@ -11,6 +11,27 @@ public class DP {
         System.out.println(climbStairs_Tab(5));
     }
 
+    public int longestCommonSubsequence(String text1, String text2) {
+        int m = text1.length(), n = text2.length();
+        int[][] LCS = new int[m + 1][n + 1];
+
+        for (int i = 0; i <= m; i++) LCS[i][0] = 0;
+        for (int j = 0; j <= n; j++) LCS[0][j] = 0;
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (text1.charAt(i-1) == text2.charAt(j-1)) {
+                    LCS[i][j] = LCS[i-1][j-1] + 1;
+                } else {
+                    int max_LCS = Math.max(LCS[i-1][j], LCS[i][j-1]);
+                    LCS[i][j] = max_LCS;
+                }
+            }
+        }
+
+        return LCS[m][n];
+    }
+
     public static int uniquePaths_tabulation(int m, int n) {
         /*
         Leet-code 62 (Tabulation)
